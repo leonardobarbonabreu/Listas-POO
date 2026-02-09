a185c4e2919ab2ee821a1a41d5ad3139756722ff
Leonardo <leonardobarbonabreu@gmail.com>
2026-02-09 19:11:25 -0300

    Agora imprime os exercícios 2 e 6

--- Lista 1/demo/src/main/java/com/example/Main.java
+++ Lista 1/demo/src/main/java/com/example/Main.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //2 Criando carro e imprimindo
+        Carro carro = new Carro("chevrollet", "camaro amarelo", LocalDate.of(2007,01,01));
+        carro.exibirDetalhes();
+
+        carro.setMarca("Fiat");
+        carro.setModelo("uno");
+        carro.setAno(LocalDate.of(2001, 12, 10));
+
+        //Exibindo um segundo carro
+        carro.exibirDetalhes();
+
+        //Instanciando um dono e relacionando-o com o carro criado anteriormente
+        Dono dono = new Dono("Pedro",20, carro);
+
     }
 }
